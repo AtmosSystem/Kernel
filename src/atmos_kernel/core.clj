3ab43aad2a8fccdd5c85dce424cc15f5a12d544d
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-02-13 19:18:49 -0400

    adding functionality

--- src/atmos_kernel/core.clj
+++ src/atmos_kernel/core.clj
@@ -1,16 +1,20 @@
 (ns atmos-kernel.core
   (:require [clojure.string :refer [lower-case capitalize]]))
 
-(defmacro defatmos-record-protocol
+(defmacro defatmos-record-protocols
   [record-name type]
-  (let [entity-lower-name# (lower-case record-name)
-        protocol-name# (symbol (str "I" (name record-name) (name type)))
+  (let [entity-name (name record-name)
+        entity-lower-name (lower-case entity-name)
+        protocol-name# #(symbol (str "I" entity-name (name %1) (name type)))
         fn-name# #(list
-                    (symbol (str (name %1) "-" entity-lower-name#))
+                    (symbol (str (name %1) "-" entity-lower-name))
                     '[value])]
-    `(defprotocol ~protocol-name#
-       ~(fn-name# :add)
-       ~(fn-name# :update)
-       ~(fn-name# :remove)
-       ~(fn-name# :get))))
+    `[(defprotocol ~(protocol-name# :Basic)
+        ~(fn-name# :add)
+        ~(fn-name# :update))
+
+      (defprotocol ~(protocol-name# :Identity)
+        ~(fn-name# :get)
+        ~(fn-name# :remove))]))
+
 
