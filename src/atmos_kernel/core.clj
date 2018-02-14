ffbe89fa0459123b8d7ceec68fd298d11a6c1a5d
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-02-14 18:47:43 -0400

    Adding web service functions

--- src/atmos_kernel/core.clj
+++ src/atmos_kernel/core.clj
@@ -1,5 +1,44 @@
 (ns atmos-kernel.core
-  (:require [clojure.string :refer [lower-case capitalize]]))
+  (:require [clojure.string :refer [lower-case capitalize join]]
+            [compojure.core :refer [GET]]
+            [compojure.route :as route]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]))
+
+;-------------------------------------------------------
+; BEGIN MICRO SERVICE FUNCTIONS
+;-------------------------------------------------------
+
+(def not-found-route (route/not-found (response {})))
+(def not-implemented-fn (route/not-found (response {:message "Not implemented method"})))
+
+(defn ms-atmos-method
+  [ms-name & params]
+  (let [ms-name (-> ms-name name lower-case)
+        params (join "/" params)]
+    (str "/"
+         ms-name
+         (if-not
+           (empty? params)
+           (str "/" params)))))
+
+(defn make-json-app
+  [routes]
+  (-> routes wrap-json-response wrap-json-body))
+
+(defn ms-atmos-main-method-response
+  [ms-name]
+  (let [ms-name (-> ms-name name lower-case)]
+    (GET "/" [] (str "Welcome to atmos " ms-name " micro-service"))))
+
+(defn ms-atmos-response
+  [data]
+  (response data))
+
+;-------------------------------------------------------
+; END MICRO SERVICE FUNCTIONS
+;-------------------------------------------------------
+
 
 (defmacro defatmos-record-protocols
   [record-name type]
