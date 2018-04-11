926026bce6252cefc077f01fae62d5bb4aac47d1
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-11 16:05:23 -0400

    [issue #1] | Adding coveralls service integration

--- test/coveralls.sh
+++ test/coveralls.sh
@@ -1,5 +1,3 @@
-COVERALLS_URL='https://coveralls.io/api/v1/jobs'
+COVERALLS_URL='https://coveralls.io/api/v1/jobs?repo_token=$COVERALLS_REPO_TOKEN'
 lein cloverage -o cov --coveralls
-echo $COVERALLS_REPO_TOKEN
-echo 'klk'
-curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
+# curl -F 'json_file=@cov/coveralls.json' "$COVERALLS_URL"
