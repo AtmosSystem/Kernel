7086eb7ecec45c5fee87b874a386a218d246bcec
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-19 17:53:21 -0400

    #30 | Fixing issue

--- src/atmos_kernel/web/route.clj
+++ src/atmos_kernel/web/route.clj
@@ -10,37 +10,37 @@
 
 (defmacro atmos-route
   "Create an atmos compojure route"
-  [http-method ms-name route-path body]
-  (let [ms-name (if-not (empty? ms-name) (-> ms-name name lower-case))
-        route-params (vec (map #(symbol (name %)) (filter keyword? route-path)))
-        route-path (let [route-path (join "/" route-path)]
-                     (str "/" ms-name (if-not (empty? route-path)
-                                        (str "/" route-path))))
-        route-params (if (seq route-params) route-params 'request)]
-    `(~http-method ~route-path
-       ~route-params
-       (atmos-response ~@body))))
+  ([http-method ms-name route-path body]
+   (let [ms-name (if-not (nil? ms-name) (-> ms-name name lower-case))
+         route-params (vec (map #(symbol (name %)) (filter keyword? route-path)))
+         route-path (let [route-path (join "/" route-path)]
+                      (str "/" ms-name (if-not (empty? route-path)
+                                         (str "/" route-path))))
+         route-params (if (seq route-params) route-params 'request)]
+     `(~http-method ~route-path
+        ~route-params
+        (atmos-response ~body)))))
 
 (defmacro atmos-GET
-  [ms-name path body]
+  [ms-name path & body]
   `(atmos-route GET ~ms-name ~path ~body))
 
 (defmacro atmos-POST
-  [ms-name path body]
+  [ms-name path & body]
   `(atmos-route POST ~ms-name ~path ~body))
 
 (defmacro atmos-PUT
-  [ms-name path body]
+  [ms-name path & body]
   `(atmos-route PUT ~ms-name ~path ~body))
 
 (defmacro atmos-DELETE
-  [ms-name path body]
+  [ms-name path & body]
   `(atmos-route DELETE ~ms-name ~path ~body))
 
 (defn atmos-main-route
   "Create the main route of web compojure application"
   ([ms-name system]
    (let [ms-name (-> ms-name name lower-case)]
-     (atmos-GET "" [] (str "Welcome to " system " " ms-name " micro-service"))))
+     (atmos-GET nil [] (str "Welcome to " system " " ms-name " micro-service"))))
   ([ms-name]
    (atmos-main-route ms-name "atmos")))
