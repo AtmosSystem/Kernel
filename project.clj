87d691a5f45221d4d7c31f855d52778465c1f819
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-20 16:51:14 -0400

    #31 | Adding functionality

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atmos-kernel "0.6.5-SNAPSHOT"
+(defproject atmos-kernel "0.6.5"
   :description "Core of Atmos System"
   :url "https://github.com/AtmosSystem/Kernel"
   :license {:name "Eclipse Public License"
