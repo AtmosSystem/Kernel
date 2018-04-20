e6191271cae518c3802e605f2fa307ce38e33987
Santiago de Pedro <santiago.pedro23@gmail.com>
2018-04-20 16:24:17 -0400

    #31 | Adding functionality

--- src/atmos_kernel/web/route.clj
+++ src/atmos_kernel/web/route.clj
@@ -10,37 +10,43 @@
 
 (defmacro atmos-route
   "Create an atmos compojure route"
-  ([http-method ms-name route-path body]
-   (let [ms-name (if-not (nil? ms-name) (-> ms-name name lower-case))
-         route-params (vec (map #(symbol (name %)) (filter keyword? route-path)))
+  ([http-method route-path body]
+   (let [route-params (vec (map #(symbol (name %)) (filter keyword? route-path)))
          route-path (let [route-path (join "/" route-path)]
-                      (str "/" ms-name (if-not (empty? route-path)
-                                         (str "/" route-path))))
+                      (str "/" (if-not (empty? route-path) route-path)))
          route-params (if (seq route-params) route-params 'request)]
      `(~http-method ~route-path
         ~route-params
         (atmos-response ~body)))))
 
 (defmacro atmos-GET
-  [ms-name path body]
-  `(atmos-route GET ~ms-name ~path ~body))
+  ([path body]
+   `(atmos-route GET ~path ~body))
+  ([body]
+   (atmos-GET [] body)))
 
 (defmacro atmos-POST
-  [ms-name path body]
-  `(atmos-route POST ~ms-name ~path ~body))
+  ([path body]
+   `(atmos-route POST ~path ~body))
+  ([body]
+   (atmos-POST [] body)))
 
 (defmacro atmos-PUT
-  [ms-name path body]
-  `(atmos-route PUT ~ms-name ~path ~body))
+  ([path body]
+   `(atmos-route PUT ~path ~body))
+  ([body]
+   (atmos-PUT [] body)))
 
 (defmacro atmos-DELETE
-  [ms-name path body]
-  `(atmos-route DELETE ~ms-name ~path ~body))
+  ([path body]
+   `(atmos-route DELETE ~path ~body))
+  ([body]
+   (atmos-DELETE [] body)))
 
 (defn atmos-main-route
   "Create the main route of web compojure application"
   ([ms-name system]
    (let [ms-name (-> ms-name name lower-case)]
-     (atmos-GET nil [] (str "Welcome to " system " " ms-name " micro-service"))))
+     (atmos-GET (str "Welcome to " system " " ms-name " micro-service"))))
   ([ms-name]
    (atmos-main-route ms-name "atmos")))
