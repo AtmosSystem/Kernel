93c0412820359dbcd03595a7fb385f2be9d8648f
Santiago de Pedro <santiago.pedro23@gmail.com>
2019-06-12 14:41:52 -0400

    Update dependencies and version
    Signed-off-by: Santiago de Pedro <sdepedro@ercbpo.com>

--- project.clj
+++ project.clj
@@ -1,20 +1,16 @@
-(defproject atmos-kernel "0.6.31"
+(defproject atmos-kernel "0.6.32-SNAPSHOT"
   :description "Core of Atmos System"
   :url "https://github.com/AtmosSystem/Kernel"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-cloverage "1.0.10"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;configuration-deps
                  [aero "1.1.3"]
                  ;web-service-deps
                  [org.clojure/data.json "0.2.6"]
-                 [clj-http "3.9.1"]
+                 [clj-http "3.10.0"]
                  [com.cemerick/url "0.1.1"]
                  [compojure "1.6.1"]
-                 [ring/ring-json "0.4.0"]
-                 [ring/ring-jetty-adapter "1.6.3"]
-                 [ring-cors "0.1.12"]
                  ;authorization-deps
                  [buddy/buddy-auth "2.1.0"]
                  ;logs-deps
