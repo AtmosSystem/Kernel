0ffa720aec52695490236da144d49302b6035a41
Santiago de Pedro <santiago.pedro23@gmail.com>
2019-07-01 02:31:54 -0400

    Adding logging Fixing auth issues

--- src/atmos_kernel/configuration.clj
+++ src/atmos_kernel/configuration.clj
@@ -1,12 +1,12 @@
 (ns atmos-kernel.configuration
-  (:require [aero.core :refer [read-config]]))
-
-(def ^:private resource-dir "resources")
+  (:require [aero.core :refer [read-config]]
+            [clojure.java.io :as io]))
 
 (defn read-resource
   "Load a resource configuration"
   [file extension]
-  (read-config (str resource-dir "/" (name file) "." (name extension))))
+  (let [source (io/resource (str (name file) "." (name extension)))]
+    (read-config source)))
 
 (defn read-edn
   "Load a edn resource"
