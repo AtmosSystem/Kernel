e45d206922a2afa0e4040467c339c15619ca2003
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-05-12 09:22:45 -0400

    Fix configuration

--- src/atmos_kernel/configuration.clj
+++ src/atmos_kernel/configuration.clj
@@ -6,13 +6,14 @@
 (defn read-resource
   "Load a resource configuration"
   [file extension]
-  (let [source (io/resource (str (name file) "." (name extension)))]
+  (if-let [source (io/resource (str (name file) "." (name extension)))]
     (read-config source)))
 
 (s/fdef read-resource
         :args (s/cat :file :atmos-kernel.spec/non-blank-string
                      :extension :atmos-kernel.spec/non-blank-string)
-        :ret :atmos-kernel.spec/non-empty-map)
+        :ret (s/or :if-not-source nil?
+                   :if-source :atmos-kernel.spec/non-empty-map))
 
 (defn read-edn
   "Load an edn resource"
@@ -21,4 +22,5 @@
 
 (s/fdef read-edn
         :args (s/cat :file :atmos-kernel.spec/non-blank-string)
-        :ret :atmos-kernel.spec/non-empty-map)
+        :ret (s/or :if-not-file nil?
+                   :if-file :atmos-kernel.spec/non-empty-map))
