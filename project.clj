789c83fbad8576c62a2a9578c3fc39d9e4df2df9
Santiago de Pedro <santiago.pedro23@gmail.com>
2020-07-21 09:44:30 -0400

    Fix issue.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject atmos-kernel "2.0"
+(defproject atmos-kernel "2.1-SNAPSHOT"
   :description "Core of Atmos System"
   :url "https://github.com/AtmosSystem/Kernel"
   :license {:name "Eclipse Public License"
@@ -9,6 +9,10 @@
                  ;pem
                  [xsc/pem-reader "0.1.1"]]
   :repositories [["releases" {:url           "https://clojars.org/repo"
+                              :username      :env/CLOJAR_USERNAME
+                              :password      :env/CLOJAR_PASSWORD
+                              :sign-releases false}]
+                 ["snapshot" {:url           "https://clojars.org/repo"
                               :username      :env/CLOJAR_USERNAME
                               :password      :env/CLOJAR_PASSWORD
                               :sign-releases false}]])
