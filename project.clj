90237a449516eaab1a7fd39b0df4a44166c39ae5
Santiago de Pedro <santiago.depedro@numerade.com>
2022-11-29 20:57:49 -0400

    Dependencies
    This commit updates the dependencies of the project.
    
    #79

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject atmos-kernel "2.1"
+(defproject atmos-kernel "2.2"
   :description "Core of Atmos System"
   :url "https://github.com/AtmosSystem/Kernel"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.11.1"]
                  ;configuration-deps
                  [aero "1.1.6"]
                  ;pem
