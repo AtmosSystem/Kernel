5021f091a080ab78e2894ad95938cc3f786605c5
Santiago de Pedro <santiago.pedro23@gmail.com>
2023-10-26 09:57:08 -0400

    Project: Version
    Bump project version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.atmos-system/atmos-kernel "2.2-SNAPSHOT"
+(defproject org.clojars.atmos-system/atmos-kernel "2.2"
   :description "Core of Atmos System"
   :url "https://github.com/AtmosSystem/Kernel"
   :license {:name "Eclipse Public License"
@@ -8,6 +8,7 @@
                  [aero "1.1.6"]
                  ;pem
                  [xsc/pem-reader "0.1.1"]]
-  :deploy-repositories [["clojars" {:url      "https://repo.clojars.org/"
+  :deploy-repositories [["clojars" {:sign-releases false
+                                    :url      "https://repo.clojars.org/"
                                     :username :env/clojars_username
                                     :password :env/clojars_password}]])
